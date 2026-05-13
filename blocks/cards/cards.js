986223719cd4386f91db4d2fb1e5b30c1cc528fc
ParasJain1803 <paras2004jain@gmail.com>
2026-05-13 15:45:52 +0530

    refactor: update cards structure and remove unnecessary class

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,7 +2,6 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 
 export default function decorate(block) {
   const ul = document.createElement('ul');
-  ul.classList.add('cards-list');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.append(...row.cloneNode(true).childNodes);
